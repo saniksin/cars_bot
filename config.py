4b8f8daf0608af7bfdb0e3b363bfc50d576c042d
Oleksandr <rbkrutoi@gmail.com>
2023-03-15 14:45:55 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+# Токен от бота
+TOKEN = '6066745993:AAHLv1sV8O-HnNSgfUWOeXEatqUOxYzxekQ'
+
+# Настройки бд
+DATABASE_USER = "xxxxx"
+DATABASE_PASSWORD = "xxxxx"
+DATABASE_HOST = "localhost"
+DATABASE_NAME = "car_db"
+
+# Сcылка на базу данных
+MYSQL_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+
